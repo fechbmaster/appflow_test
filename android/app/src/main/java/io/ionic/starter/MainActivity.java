b0ae932c367dfdbe534bba7e01b844c0d80e59aa
Bernd Fecht <bernd.fecht@ghostthinker.de>
2020-12-04 11:14:50 +0100

    Android visual app center support

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -7,10 +7,20 @@ import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    AppCenter.start(getApplication(), "45a78c44-f44f-4cc2-8d82-78f09c68bf83",
+                      Analytics.class, Crashes.class);
 
     // Initializes the Bridge
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
